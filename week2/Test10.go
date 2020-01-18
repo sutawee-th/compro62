08d59ffbcba0385281cebf4a7cb027ce9453f54e
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-01-19 00:55:41 +0700

    Create Test10.go

---
+++ week2/Test10.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.HasPrefix("Hello World", "hello"))
+	fmt.Println(strings.HasPrefix("Hello World", "Hello"))
+}
