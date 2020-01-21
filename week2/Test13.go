d0aa6ad582f0d0ec390c070374f44bc212990f8d
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-01-22 01:27:30 +0700

    Create Test13.go

---
+++ week2/Test13.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Replace("Hello World", "l", "x", 2))
+	fmt.Println(strings.Replace("Hello World", "l", "x", -1))
+}
