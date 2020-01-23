42534d9e39f39b2291a78f0f0071a0ef5798c276
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-01-23 15:55:18 +0700

    Create Test1.go

---
+++ week3/Test1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	n, e := fmt.Print("Hello", "World", 123, 456, "\n")
+	fmt.Print("number of bytes written :", n, "\n")
+	fmt.Print("write error encountered :", e, "\n")
+}
