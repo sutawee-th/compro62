e8f37f10e965829986b1be9de5996c76ef4b0eaa
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-01-23 17:54:31 +0700

    Create Test4.go

---
+++ week3/Test4.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("inut : ")
+	var name string
+	var age int
+	var height float32
+	var weight float32
+	n, err := fmt.Scan(&name, &age, &weight, &height)
+	fmt.Println(name, age, weight, height)
+	fmt.Println(`number of argument `, n)
+	fmt.Println(`error  `, err)
+}
