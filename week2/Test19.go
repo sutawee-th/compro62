972ff2c8027086c3bae870510343627a5825f978
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-01-23 15:55:15 +0700

    Create Test19.go

---
+++ week2/Test19.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("10 is of type %T\n", 10)
+
+	fmt.Printf("10 is of type %T\n", string(10))
+
+	fmt.Printf("10 is of type %T\n", float64(10))
+}
