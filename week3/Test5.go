4fe80a48152d8731371c14fff5b24d04939b03d4
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-01-23 18:16:14 +0700

    Create Test5.go

---
+++ week3/Test5.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var f float64
+	var d int
+	n, e := fmt.Scanf("%E %d", &f, &d)
+	fmt.Println("read floate ", f)
+	fmt.Println("read int ", d)
+	fmt.Println("number of items successfully scanned", n)
+	fmt.Println("error ", e)
+}
