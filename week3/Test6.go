d0832a679294db8abb774028ade20f2f1de8a9e5
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-02-08 16:27:33 +0700

    Create Test6.go

---
+++ week3/Test6.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+	n, e := fmt.Scanln(&name, &age)
+	fmt.Println("name ", name)
+	fmt.Println("age ", age)
+	fmt.Println("number of items successfully scanned ", n)
+	fmt.Println("error ", e)
+}
