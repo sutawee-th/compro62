dd79d35bb7f6bf44d224bd2af58e15b0430040ff
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-02-09 15:40:15 +0700

    Update array.go

--- week5/array.go
+++ week5/array.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	student := []string{"Goku", "Gohan", "Vegeta", "Piccolo", "Krillin"
+	student := []string{"Goku", "Gohan", "Vegeta", "Piccolo", "Krillin",
 		"Yamcha", "Bluma", "Videl", "Oolong", "Puar"}
 
 	fmt.Println(student)
