04d2ef6fce6fff389166bd920fa82e09cb37897a
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-02-16 02:03:49 +0700

    Update Test17.go

--- week5/Test17.go
+++ week5/Test17.go
@@ -1,7 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	humans := []string{"Bulma", "chi-chi", "Videl"}
 	names := []string{"Goku", "Gohan"}
 	names = append(names, humans...)
+	fmt.Println(names)
 }
