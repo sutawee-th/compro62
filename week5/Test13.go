91bdbc5f232becb05037af0d60f43cb5dc79be9b
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-02-16 00:34:50 +0700

    Update Test13.go

--- week5/Test13.go
+++ week5/Test13.go
@@ -1,1 +1,14 @@
 package main
+import "fmt"
+
+func main() {
+	x := make([]int, 5)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	y := make([]int, 5, 10)
+	fmt.Println(y)
+	fmt.Println(len(y))
+	fmt.Println(cap(y))
+}
