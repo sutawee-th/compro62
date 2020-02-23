09b01850915d7017ddeafb482bf451602aa095fc
sutawee-th <59304512+sutawee-th@users.noreply.github.com>
2020-02-23 23:25:43 +0700

    Update codstest.go

--- work.go/codstest.go
+++ work.go/codstest.go
@@ -1,10 +1,14 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 func readCurrentDir() {
 	file, err := os.Create("output.txt")
 	if err != nil {
-
+		log.Fatalf("failed opening directory")
 	}
+	defer file.Close()
 }
